c1dea027ef9daed0af1938b320d544022d0795dc
ebornm21 <115953763+ebornm21@users.noreply.github.com>
2024-04-20 02:52:25 -0500

    Update bubblewrap.js
    Added pop sound and make it play when a bubble is clicked.

--- bubblewrap.js
+++ bubblewrap.js
@@ -6,9 +6,12 @@ for (i=0; i<pops.length; i++) {
     pops[i].id = i;
 };
 
+var popSound = new Audio("./media/pop.mp3")
+
 $("td").click(function (event) {
     var pop = event.target.id;
     $("#" + pop).removeClass("covered");
+    popSound.play();
 });
 
 $("#reset").click(function () {
