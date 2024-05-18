a887226f5918d2c0e2ca3d8ea55ad288676e6562
ebornm21 <115953763+ebornm21@users.noreply.github.com>
2024-05-17 20:27:42 -0500

    Update gridpainting.js
    Added functionality to select and use multiple different colors and added a mouseover animation to show where the cursor is currently selecting.

--- gridpainting.js
+++ gridpainting.js
@@ -1,8 +1,24 @@
-$("td").click(function (event) {
+var currentColor =  "colored-plum";
+
+$("#palette td").click(function(event) {
+    currentColor = event.currentTarget.className;
+    console.log(currentColor);
+})
+
+$("#grid td").hover(function(event) {
     var pixel = event.currentTarget;
-    $(pixel).addClass("colored");
+    $(pixel).addClass(currentColor + "-opaque");
+}, function(event) {
+    var pixel = event.currentTarget;
+    $(pixel).removeClass(currentColor + "-opaque");
+})
+
+$("#grid td").click(function (event) {
+    var pixel = event.currentTarget;
+    $(pixel).removeClass();
+    $(pixel).addClass(currentColor);
 })
 
 $("#reset").click(function () {
-    $("td").removeClass("colored");
+    $("#grid td").removeClass();
 })
